a3626311d66743208588e43c790edc698b41d355
Kevin Gallego <kevinelevengallego@gmail.com>
2024-06-19 08:43:49 -0500

    update files

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import './style.css';
 
+const JSONBIN_URL = 'https://api.jsonbin.io/v3/b/6672da4cacd3cb34a859d109'; // Reemplaza <tu_jsonbin_id> con el ID de tu JSONBin
+
 // VARIABLES
 loadListeners();
 
@@ -12,21 +14,32 @@ function loadListeners() {
 
 // FUNCTIONS
 async function readTasks() {
-  const { data } = await axios.get('/api/tasks');
-  const tbody = document.querySelector('#tasks-table tbody');
-  tbody.innerHTML = '';
-  data.forEach(task => {
-    const row = document.createElement('tr');
-    row.innerHTML = `
-      <td>${task.title}</td>
-      <td>${task.author}</td>
-      <td><button data-id="${task.id}">Delete</button></td>
-    `;
-    tbody.appendChild(row);
-  });
-  document.querySelectorAll('button[data-id]').forEach(button => {
-    button.addEventListener('click', deleteTask);
-  });
+  try {
+    const { data } = await axios.get(JSONBIN_URL, {
+      headers: {
+        'secret-key': '$2a$10$SBr5PzXiwwXHafVuv7x4Duddw9bppVFfXpOU/GG3EyEYtUBwrEM6C'
+      }
+    });
+
+    const tbody = document.querySelector('#tasks-table tbody');
+    tbody.innerHTML = '';
+    
+    data.task.forEach(task => {
+      const row = document.createElement('tr');
+      row.innerHTML = `
+        <td>${task.title}</td>
+        <td>${task.author}</td>
+        <td><button data-id="${task.id}">Delete</button></td>
+      `;
+      tbody.appendChild(row);
+    });
+
+    document.querySelectorAll('button[data-id]').forEach(button => {
+      button.addEventListener('click', deleteTask);
+    });
+  } catch (error) {
+    console.error('Error al leer las tareas:', error);
+  }
 }
 
 async function createTask(event) {
@@ -34,15 +47,34 @@ async function createTask(event) {
   const title = document.getElementById('task-title').value;
   const author = document.getElementById('task-author').value;
 
-
   const taskToCreate = { title, author };
-  const { data } = await axios.post('/api/tasks', taskToCreate);
-  document.getElementById('task-form').reset();
-  readTasks();
+
+  try {
+    await axios.post(JSONBIN_URL, taskToCreate, {
+      headers: {
+        'secret-key': '$2b$10$<tu_secret_key>'
+      }
+    });
+    
+    document.getElementById('task-form').reset();
+    readTasks();
+  } catch (error) {
+    console.error('Error al crear la tarea:', error);
+  }
 }
 
 async function deleteTask(event) {
   const idToTaskDelete = event.target.getAttribute('data-id');
-  await axios.delete(`/api/tasks?id=${idToTaskDelete}`);
-  readTasks();
+
+  try {
+    await axios.delete(`${JSONBIN_URL}/${idToTaskDelete}`, {
+      headers: {
+        'secret-key': '$2b$10$<tu_secret_key>'
+      }
+    });
+
+    readTasks();
+  } catch (error) {
+    console.error('Error al eliminar la tarea:', error);
+  }
 }
